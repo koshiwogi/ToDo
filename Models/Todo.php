db2640706dbb8957f9e5e3a5e22246980f102e46
柏木孝洋 <kashiwagitakahiro@kashiwagitakahironoMacBook-Air.local>
2019-08-01 12:01:24 +0800

    今日はここまで

--- Models/Todo.php
+++ Models/Todo.php
@@ -1,3 +1,33 @@
 <?php
 
 require_once('config/dbconnect.php');
+
+//todo
+class Todo
+{   //プロパティー
+    private $table = 'tasks';
+    private $db_manager;
+
+    //インスタンス化した時に呼ばれるメソッド
+    public function __construct()
+    {       //db_manageプロパティーは
+        //DbManagerクラスのインスタンス
+        //dbhプロパティーの
+        //pdoのインスタンス
+        //prepareメッソドを実行
+        //クラスは一つのことを実行すればいい　//シングルタスク
+        $this->db_manager = new DbManager();
+        $this->db_manager->connect();
+    }
+
+    public function create($name)
+    {
+        // dbに保存
+        //このクラスのインスタンスの
+        //db_managerプロパティーの
+        //dbhプロパティーの
+        //prepareメソッドを実行
+        $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . ' (name) VALUES (?)');
+        $stmt->execute([$name]);
+    }
+}
--- create.php
+++ create.php
@@ -1,10 +1,26 @@
 <?php
+//Todo.phpを読み込む
+require_once('Models/Todo.php');
+//ユーザーが入力した内容を取得
+//$taskっていう変数にユーザーが入力した内容代入
+//$task = ユーザーがにゅうりょくした内容
+$task = $_POST['task'];
 
-//addボタンをしたら
-//ユーザーが入力した内容をdbに保存して
+//dbに保存
+
+
+//todo クラスをインンスタンス化して$todoに代入
+$todo = new Todo();
+
+
+//todoクラスのインスタンス
+//createメソッドを実行
+$todo->create($task);
+
+
 
 
 //一覧画面に戻る
 header('location: index.php');
 
-//関数名（引数、引数、引数）;
+//関数名（引数、引数、引数）;gid
